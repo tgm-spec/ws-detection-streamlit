fc132776b54d56758a4ab07194965d3b25f86edb
Srijib Dey <srijibdey24@gmail.com>
2026-03-06 10:12:29 +0530

    improved model loading

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 import streamlit as st
 
 
-# Cache the model so Streamlit doesn't reload it every time
 @st.cache_resource
 def load_model():
 
-    model_path = "models/wsV3.keras"
+    with st.spinner("Loading AI model..."):
 
-    model = tf.keras.models.load_model(model_path)
+        model_path = "models/wsV3.keras"
+
+        model = tf.keras.models.load_model(model_path)
 
     return model
